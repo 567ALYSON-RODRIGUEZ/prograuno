92421568bada8a0b005b05d223a3a8c545a354bd
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-01-30 22:54:07 -0600

    suma

---
+++ Untitled1.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Suma = 0;
+    cout << "Introduce los valores que deseas sumar";
+    cin >> Suma;
+    cout << endl << "La suma de los valores es:" << Suma << endl;
+
+    return 0;
+}
